6d1a8825a8b606296e1fe202cee1296fc4c5f73f
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-07-13 20:18:55 +0200

    Printout all definitions in schema

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
-  match openapi::from_path("src/swagger.json") {
-    Ok(spec) => println!("spec: {:?}", spec),
+  let args: Vec<String> = std::env::args().collect();
+  let path_to_openapi_def = args.get(1).map_or("error", |x|x);
+  match openapi::from_path(path_to_openapi_def) {
+    Ok(spec) => {
+      for (name, schema) in spec.definitions.into_iter() {
+        println!("name: {:?}", name);
+        println!("spec: {:?}", schema);
+      };
+    },
     Err(err) => println!("error: {}", err)
   }
 }
