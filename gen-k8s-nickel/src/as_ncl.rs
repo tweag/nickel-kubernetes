cedd86e959abf173d956d0366d699dcac7ce0eb5
Aleksander Gondek <gondekaleksander@protonmail.com>
2021-07-19 19:13:34 +0200

    Rustmft on source-code

--- gen-k8s-nickel/src/as_ncl.rs
+++ gen-k8s-nickel/src/as_ncl.rs
@@ -11,10 +11,16 @@ fn k8s_ref_to_ncl_id(r: &str) -> String {
 }
 
 fn _to_ref_def(name: &str, indent: usize, ref_path: &Option<String>) -> String {
-  ref_path.as_ref().map_or(
-    format!("<sensible_warning:{}>", name),
-    |r| format!("{}{} | {}", format_args!("{:>1$}", "", indent), name, k8s_ref_to_ncl_id(r.as_str()))
-  )
+  ref_path
+    .as_ref()
+    .map_or(format!("<sensible_warning:{}>", name), |r| {
+      format!(
+        "{}{} | {}",
+        format_args!("{:>1$}", "", indent),
+        name,
+        k8s_ref_to_ncl_id(r.as_str())
+      )
+    })
 }
 
 fn _to_str_def(name: &str, indent: usize) -> String {
@@ -22,34 +28,68 @@ fn _to_str_def(name: &str, indent: usize) -> String {
 }
 
 fn _to_int_def(name: &str, indent: usize) -> String {
-  format!("{}{} | #(nums.Int)", format_args!("{:>1$}", "", indent), name)
+  format!(
+    "{}{} | #(nums.Int)",
+    format_args!("{:>1$}", "", indent),
+    name
+  )
 }
 
-fn _to_list_def(name: &str, indent: usize, items: &Option<Box<openapi::Schema>>) -> String {
-  let item_type = items.as_ref().map_or(format!("<sensible_warning:{}>", name), |i| {
-    i.ref_path.as_ref().map_or(
-      format!("<sensible_warning:{}>", name),
-      |r| k8s_ref_to_ncl_id(r)
-    )
-  }); 
-  format!("{}{} | List {}", format_args!("{:>1$}", "", indent), name, item_type)
+fn _to_list_def(
+  name: &str,
+  indent: usize,
+  items: &Option<Box<openapi::Schema>>,
+) -> String {
+  let item_type =
+    items
+      .as_ref()
+      .map_or(format!("<sensible_warning:{}>", name), |i| {
+        i.ref_path
+          .as_ref()
+          .map_or(format!("<sensible_warning:{}>", name), |r| {
+            k8s_ref_to_ncl_id(r)
+          })
+      });
+  format!(
+    "{}{} | List {}",
+    format_args!("{:>1$}", "", indent),
+    name,
+    item_type
+  )
 }
 
-fn _to_custom_type_def(name: &str, indent: usize, properties: &Option<std::collections::BTreeMap<String, openapi::Schema>>) -> String {
+fn _to_custom_type_def(
+  name: &str,
+  indent: usize,
+  properties: &Option<std::collections::BTreeMap<String, openapi::Schema>>,
+) -> String {
   if properties.as_ref().is_none() {
-    format!("{}{} | #(utils.DictStrStr)", format_args!("{:>1$}", "", indent), name)
+    format!(
+      "{}{} | #(utils.DictStrStr)",
+      format_args!("{:>1$}", "", indent),
+      name
+    )
   } else {
-    let props_defs = properties.as_ref().map_or(
-      format!("<sensible_warning:{}>", name), 
-      |p| {
-        p.into_iter().map(|p| {
-          let (n, s) = p;
-          s.to_ncl(n.as_str(), indent + 2)
-        }).collect::<Vec<String>>()
-        .join(",\n") + ","
-      }
-    );
-    format!("{}{} = {{\n{}\n{}}}", format_args!("{:>1$}", "", indent), name, props_defs, format_args!("{:>1$}", "", indent))
+    let props_defs =
+      properties
+        .as_ref()
+        .map_or(format!("<sensible_warning:{}>", name), |p| {
+          p.into_iter()
+            .map(|p| {
+              let (n, s) = p;
+              s.to_ncl(n.as_str(), indent + 2)
+            })
+            .collect::<Vec<String>>()
+            .join(",\n")
+            + ","
+        });
+    format!(
+      "{}{} = {{\n{}\n{}}}",
+      format_args!("{:>1$}", "", indent),
+      name,
+      props_defs,
+      format_args!("{:>1$}", "", indent)
+    )
   }
 }
 
@@ -65,7 +105,7 @@ impl AsNcl for openapi::Schema {
         "array" => _to_list_def(name, indent, &self.items),
         "integer" => _to_int_def(_k8s_name, indent),
         "string" => _to_str_def(_k8s_name, indent),
-        _ => format!("<sensible_warning:{}>", _k8s_name)
+        _ => format!("<sensible_warning:{}>", _k8s_name),
       },
     )
   }
--- gen-k8s-nickel/src/main.rs
+++ gen-k8s-nickel/src/main.rs
@@ -1,13 +1,13 @@
-use tweag_gen_k8s_nickel::as_ncl::{AsNcl};
+use tweag_gen_k8s_nickel::as_ncl::AsNcl;
 
 fn main() {
   let args: Vec<String> = std::env::args().collect();
-  let path_to_openapi_def = args.get(1).map_or("error", |x|x);
+  let path_to_openapi_def = args.get(1).map_or("error", |x| x);
   match openapi::from_path(path_to_openapi_def) {
     Ok(spec) => {
       for (name, schema) in spec.definitions.into_iter() {
         if name != "io.k8s.apimachinery.pkg.apis.meta.v1.ObjectMeta" {
-          continue
+          continue;
         }
         let mut a = "let ".to_string();
         let b = schema.to_ncl(name.as_str(), 0);
@@ -15,8 +15,8 @@ fn main() {
         a.push_str("\n");
 
         std::fs::write("result.ncl", a);
-      };
-    },
-    Err(err) => println!("error: {}", err)
+      }
+    }
+    Err(err) => println!("error: {}", err),
   }
 }
